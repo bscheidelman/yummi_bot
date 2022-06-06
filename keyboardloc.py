ac2064eadcd7e6748110edaa61f08b2cdf49bb81
Ben Scheidelman <55633516+bscheidelman@users.noreply.github.com>
2022-06-06 18:04:59 -0500

    Update keyboardloc.py

--- keyboardloc.py
+++ keyboardloc.py
@@ -1,8 +1,16 @@
 import pyautogui
 import time
+import PIL.ImageGrab
+
+
+def findRGB(xloc,yloc):
+    rgb = PIL.ImageGrab.grab().load()[xloc,yloc]
+    return rgb
 
 temp = True
 while temp:
     x = pyautogui.position()
+
     print(x)
+    print(findRGB(x.x,x.y))
     time.sleep(2.5)
